2b276351acae7c2656dd811a35b696f5988d05db
unknown <asifvg@code.edu.az>
2021-11-24 11:52:23 +0400

    add new files

---
+++ FrontToBack/FrontToBack/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using FrontToBack.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBack.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Home";
+            return View();
+        }
+
+        
+    }
+}
---
+++ FrontToBack2/WebApplication1/WebApplication1/Controllers/LogInController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class LogInController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "LogIn";
+            return View();
+        }
+    }
+}
